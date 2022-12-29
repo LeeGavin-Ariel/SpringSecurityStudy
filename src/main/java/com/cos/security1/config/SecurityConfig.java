b88a5cb6b899a3b38b3d826067b85929b984daa6
LeeGavin <kv829ly@naver.com>
2022-12-29 22:54:37 +0900

    [강의] Spring Security - OAuth Google Login 기본 세팅

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.cos.security1.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,12 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.cos.security1.config.oauth.PrincipalOauth2UserService;
+
+
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 // secured 어노테이션 활성화, preAuthorize & postAuthorize 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	
+	@Autowired
+	private PrincipalOauth2UserService principalOauth2UserService;
 	
 	// 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
 	@Bean
@@ -34,7 +42,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.loginPage("/loginForm")
 			.usernameParameter("userName")
 			.loginProcessingUrl("/login") // /login이라는 주소가 호출되면 시큐리티가 낚아채서 대신 로그인 진행
-			.defaultSuccessUrl("/"); // loginForm을 통해서 로그인을 하면 내가 가려고 하는 페이지로 이동시켜줌
+			.defaultSuccessUrl("/") // loginForm을 통해서 로그인을 하면 내가 가려고 하는 페이지로 이동시켜줌
 			// 만약 /user에서 로그인을 했으면 /user로 보내줌
+			.and()
+			.oauth2Login()
+			.loginPage("/loginForm") // 해당 페이지에서 oauth 로그인을 진행하겠다.
+			// 구글 로그인이 된 뒤의 후처리
+			//1. 코드받기(인증)
+			//2. 엑세스토큰(사용자 정보에 접근할 권한이 생김)
+			//3. 사용자 프로필 정보를 가져옴
+			//4-1. 정보를 토대로 회원가입을 자동으로 진행시키기도 함
+			//4-2. 정보가 모자랄 경우 추가 정보 기입 
+			.userInfoEndpoint()
+			//Tip! 구글 로그인이 완료가 되면 코드를 받는 것이 아니라, 엑세스토큰+사용자 프로필정보를 한방에 받는다.
+			.userService(principalOauth2UserService);
 	}
 }
